7b47ee36ebc0acf864b587b3adcef98dff96400d
HS <h.sekmenoglu@12mconsulting.com.tr>
2018-02-19 14:51:45 +0300

    extensions added

---
+++ Birikim.Core/Extensions/ComparableExtensions.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Birikim.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="IComparable{T}"/>.
+    /// </summary>
+    public static class ComparableExtensions
+    {
+        /// <summary>
+        /// Checks a value is between a minimum and maximum value.
+        /// </summary>
+        /// <param name="value">The value to be checked</param>
+        /// <param name="minInclusiveValue">Minimum (inclusive) value</param>
+        /// <param name="maxInclusiveValue">Maximum (inclusive) value</param>
+        public static bool IsBetween<T>(this T value, T minInclusiveValue, T maxInclusiveValue) where T : IComparable<T>
+        {
+            return value.CompareTo(minInclusiveValue) >= 0 && value.CompareTo(maxInclusiveValue) <= 0;
+        }
+
+        /// <summary>
+        /// Indicates whether this string is null or an System.String.Empty string.
+        /// </summary>
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+        /// <summary>
+        /// indicates whether this string is null, empty, or consists only of white-space characters.
+        /// </summary>
+        public static bool IsNullOrWhiteSpace(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+    }
+}
---
+++ Birikim.Core/Extensions/IntegerExtensions.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Birikim.Extensions
+{
+    /// <summary>
+    /// Extension methods for Integer class.
+    /// </summary>
+    public static class IntegerExtensions
+    {
+        /// <summary>
+        /// Nicely formatted file size. This method will return file size with bytes, KB, MB and GB in it. You can use this alongside the Extension method named FileSize.
+        /// </summary>
+        public static string FormatFileSize(this int fileSize)
+        {
+            //declarations
+            string[] suffix = { "B", "KB", "MB", "GB", "TB" };
+            var j = 0;
+
+            //loop and divide
+            while (fileSize > 1024 && j < (suffix.Length - 1))
+            {
+                fileSize = fileSize / 1024;
+                j++;
+            }
+
+            // Adjust the format string to your preferences. For example "{0:0.#}{1}" would
+            // show a single decimal place, and no space.
+            return string.Format("{0:0} {1}", fileSize, suffix[j]);
+        }
+
+        /// <summary>
+        /// Convert OaDate to DateTime
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static DateTime FromOaDate(this int value)
+        {
+            try
+            {
+                return DateTime.FromOADate(value).Date;
+            }
+            catch (Exception)
+            {
+                return DateTime.Today.Date;
+            }
+        }
+
+        /// <summary>
+        /// Convert OaDate to String date format
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="Format"></param>
+        /// <returns></returns>
+        public static string FromOADate(this int value, string Format = "")
+        {
+            try
+            {
+                return DateTime.FromOADate(value).ToString(Format);
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Convert OaTime to String date format
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string FromOaTime(this int value)
+        {
+            try
+            {
+                var now = new DateTime(2000, 1, 1);
+                return now.AddSeconds(value).TimeOfDay.ToString();
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Conevrts object to integer
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static int ToInt32(this object value)
+        {
+            try { return Convert.ToInt32(value); }
+            catch { return 0; }
+        }
+    }
+}
