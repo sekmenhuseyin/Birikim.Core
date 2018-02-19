253223c1a7b1f6a5878e1a33266665f3d6c83cd9
HS <h.sekmenoglu@12mconsulting.com.tr>
2018-02-19 14:53:12 +0300

    helpers added

---
+++ Birikim.Core/Constants.cs
@@ -0,0 +1,23 @@
+﻿namespace Birikim
+{
+    /// <summary>
+    ///     Used to define some constants
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        ///     Source name of framework.
+        /// </summary>
+        public const string SourceName = "Birikim";
+
+        /// <summary>
+        /// List of Temporary Email Services
+        /// </summary>
+        public static string[] TemporaryEmailServices = { "@mswork", "@leeching", "@extremail", "@kismail", "@divismail", "@sharklasers", "@grr", "@guerrillamail", "@spam", "@turoid", "temp", "@trbvn", "@thraml", "@vvx", "@yomail", "@dropmail", "@10mail", "@emltmp", "@cetesavaslari", "@mailinator" };
+
+        /// <summary>
+        /// Most used passwords
+        /// </summary>
+        public static string[] MostUsedPasswords = { "11111", "111111", "1111111", "12345", "123456", "1234567", "12345678", "123456789", "654321", "1qaz2wsx", "password", "iloveyou", "princess", "football", "baseball", "rockyou", "abc123", "nicole", "daniel", "babygirl", "monkey", "master", "admin", "login", "jessica", "lovely", "michael", "ashley", "winter", "summer", "qwerty", "qwertyuiop", "sifre", "welcome", "welcome1", "welcome2", "welcome01", "passw0rd", "password1", "password2", "password3", "password4", "password6", "password7", "password8", "password9", "password01", "password123" };
+    }
+}
